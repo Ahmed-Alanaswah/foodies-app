44741126e4789d2f78e2a6a2c2d0f631fc20af3c
Ahmed-Alanaswah <alanaswehahmed0@gmail.com>
2026-04-01 18:34:12 +0300

    add loading

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,35 @@
+import Link from "next/link";
+import MealsGrid from "@/components/meals/meals-grid";
+import classes from "./page.module.css";
+import { getMeals } from "@/lib/meal";
+import { Suspense } from "react";
+
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
 export default function MealsPage() {
   return (
     <>
-      <header className={classes.header}></header>
-      <main className={classes.main}></main>
+      <header className={classes.header}>
+        <h1>
+          Delecious meal created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          choose your favorite recipe and cook it yourself, it is easy and fun.
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense
+          fallback={<p className={classes.loading}>Loading meals...</p>}
+        >
+          <Meals />
+        </Suspense>
+      </main>
     </>
   );
 }
